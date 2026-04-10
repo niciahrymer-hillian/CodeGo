e0c76da6d8589a2b8214b47668cd5b5d44c06522
niciahrymer-hillian <rymer.niciah@gmail.com>
2026-04-10 19:19:52 -0400

    Add Python Variable guide

---
+++ python/vars.py
@@ -0,0 +1,18 @@
+>>> age = 25
+>>> age 
+25
+>>> score = 100
+>>> score
+100
+>>> negative = -10
+>>> negative
+-10
+>>> sum_total = age + score
+>>> sum_total
+125
+>>> dir()
+['__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__', 'age', 'negative', 'score', 'sum_total']
+>>> globals()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__builtins__': <module 'builtins' (built-in)>, 'age': 25, 'score': 100, 'negative': -10, 'sum_total': 125}
+>>> 
+
