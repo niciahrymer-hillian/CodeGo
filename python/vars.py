117c6eabf48f0c2fb13d4cb540668b539bbc1887
niciahrymer-hillian <rymer.niciah@gmail.com>
2026-04-10 19:32:28 -0400

    Add Python Variables Guide completed

--- python/vars.py
+++ python/vars.py
@@ -16,3 +16,59 @@
 {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__builtins__': <module 'builtins' (built-in)>, 'age': 25, 'score': 100, 'negative': -10, 'sum_total': 125}
 >>> 
 
+>>> is_student = True
+>>> is_student
+True
+>>> has_passed_exam = False
+>>> has_passesd_exam
+Traceback (most recent call last):
+  File "<python-input-3>", line 1, in <module>
+    has_passesd_exam
+NameError: name 'has_passesd_exam' is not defined. Did you mean: 'has_passed_exam'?
+>>> has_passed_exam = False
+>>> has_passed_exam
+False
+>>> result = 10 > 5
+>>> result
+True
+>>> is_equal = (15 == 15)
+>>> is_equal
+True
+>>> price = 19.99
+>>> price
+19.99
+>>> temperature = -2.5
+>>> temperature
+-2.5
+>>> result = 10.5 + 3.7
+>>> result
+14.2
+>>> division = 10.0 / 3.0
+>>> division
+3.3333333333333335
+>>> name = "John"
+>>> name
+'John'
+>>> greeting = "Hello, World!"
+>>> greeting
+'Hello, World!'
+>>> combined = name + " says " + greeting
+>>> combined
+'John says Hello, World!'
+>>> empty = ""
+>>> empty
+''
+>>> formatted = f"{name} says {greeting}"
+>>> formatted
+'John says Hello, World!'
+>>> x = 5
+>>> type(x)
+<class 'int'>
+>>> x = "hello"
+>>> type(x)
+<class 'str'>
+>>> x = 3.14
+>>> type(x)
+<class 'float'>
+>>> 
+
